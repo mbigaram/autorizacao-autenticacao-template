a690641a06f46b2b6e9d5514266cbbfa52a8a155
mbiga <mbigaram.mb@gmail.com>
2022-11-28 15:01:36 -0300

    pratica guiada

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,6 +1,38 @@
+import axios from "axios";
+import { useEffect } from "react";
 import Header from "../components/Header";
+import { navigateToHome } from "../routes/coordinator";
 
 function AdminPage() {
+
+
+  // const token = localStorage.getItem("token")
+  // const navigate = useNavigate()
+
+  // useEffect(()=> {
+
+  //   if(!token){
+  //     navigateToHome(navigate)
+  //   }
+  // })
+
+  
+
+  useEffect(()=>{
+    const aluno = "darvas"
+    const id = "3bUbdB1gvPzWrThpazVC"
+    const header = {headers: {
+      auth: localStorage.getItem("token")
+    }}
+
+    axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labeX/${aluno}/trip/${id}`,header)
+    .then((resp)=>{
+      console.log("resposta do admin", resp.data.trip)
+    })
+    .catch((erro)=>{
+      console.log(erro)
+    })
+  },[])
   return (
     <main>
       <Header />
